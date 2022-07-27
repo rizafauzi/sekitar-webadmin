2524d7efce8cb57a3c2b2edcb48306197c2e562a
rizafauzir <97901303+rizafauzir@users.noreply.github.com>
2022-07-27 19:51:08 +0700

    fix: csv format

--- src/features/Order/components/ExportCSVModal.tsx
+++ src/features/Order/components/ExportCSVModal.tsx
@@ -136,17 +136,22 @@ const ExportCsv: React.FC = () => {
             'order_state',
             'courier_id',
             'courier_name',
-            'courier_area'
+            'courier_area',
+            'store_name',
+            'buyer_name'
           ]
           const sanitizedBody = response?.data
-            .replace('date,hour,cart_id,order_state,courier_id,courier_name,courier_area', '')
+            .replace(
+              'date,hour,cart_id,order_state,courier_id,courier_name,courier_area,store_name,buyer_name',
+              ''
+            )
             .replaceAll('\n', '')
             .split(',')
           let finalPayload = [header]
           let temp = []
           for (let i = 1; i < sanitizedBody.length; i++) {
             temp.push(sanitizedBody[i - 1])
-            if (i % 6 === 0) {
+            if (i % 8 === 0) {
               finalPayload.push(temp)
               temp = []
             }
