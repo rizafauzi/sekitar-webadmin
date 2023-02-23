123eb1e12146d95259690be1a40065c15355263e
GannaHup <gannahap02@gmail.com>
2023-02-23 20:48:21 +0700

    feat: search able bank account

--- src/features/BankAccount/api/index.ts
+++ src/features/BankAccount/api/index.ts
@@ -1,16 +1,20 @@
+/* eslint-disable object-shorthand */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable unicorn/prevent-abbreviations */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { apiRequest } from '@configs/axios'
 import sekitarEnv from '@utils/ENV'
 import { IBankAccountListParams } from '../BankAccount.type'
 
-export const getBankAccountList = ({ page, limit }: IBankAccountListParams) =>
+export const getBankAccountList = ({ page, limit, search }: IBankAccountListParams) =>
   apiRequest({
-    path: '/api/dashboard/stores/bank_list',
+    path: '/api/dashboard/stores/bank_list/search',
     method: 'GET',
     params: {
       p: page,
-      c: limit
+      c: limit,
+      name: search || ''
     },
     headers: {
       access_token: sekitarEnv.merchantToken
