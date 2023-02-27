a37396d5eb2436efb92f63f48c1759908ed831c8
GannaHup <gannahap02@gmail.com>
2023-02-27 23:08:52 +0700

    feat: integration detail & edit bank account

--- src/features/BankAccount/hooks/index.ts
+++ src/features/BankAccount/hooks/index.ts
@@ -1,9 +1,16 @@
-/* eslint-disable import/prefer-default-export */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable unicorn/prevent-abbreviations */
 import { ApiResponse } from '@configs/axios'
-import { useQuery } from 'react-query'
-import { getBankAccountList } from '../api'
-import { IBankAccountList, IBankAccountListParams } from '../BankAccount.type'
+import { useMutation, UseMutationOptions, useQuery, UseQueryOptions } from 'react-query'
+import { getAllBank, getBankAccountList, getDetailBankAccount, updateBankAccount } from '../api'
+import {
+  DetailBankAccountResponse,
+  IBankAccountList,
+  IBankAccountListParams,
+  IBankList,
+  UpdateBankAccountRequest
+} from '../BankAccount.type'
 
 export const useFetchBankAccountList = (params: IBankAccountListParams) => {
   const { data, isError, isLoading, refetch } = useQuery(
@@ -24,3 +31,38 @@ export const useFetchBankAccountList = (params: IBankAccountListParams) => {
     refetch
   }
 }
+
+export const useFetchAllBank = (params: IBankAccountListParams) => {
+  const { data, isError, isLoading, refetch } = useQuery(['bank-list', params], async () => {
+    const response: ApiResponse<IBankList[]> = await getAllBank(params)
+    return response.data.Data.map((item: IBankList, index: number) => ({
+      ...item,
+      index: index + 1
+    }))
+  })
+
+  return {
+    isLoading,
+    isError,
+    data,
+    refetch
+  }
+}
+
+export const useFetchDetailBankAccount = (
+  storeId: number,
+  options?: UseQueryOptions<DetailBankAccountResponse>
+) =>
+  useQuery({
+    queryKey: ['detail-bank-account', storeId],
+    queryFn: () => getDetailBankAccount(storeId),
+    ...options
+  })
+
+export const useUpdateBankAccount = (
+  options?: UseMutationOptions<unknown, Error, UpdateBankAccountRequest>
+) =>
+  useMutation({
+    mutationFn: data => updateBankAccount(data),
+    ...options
+  })
--- src/features/BankAccount/screens/Detail/index.tsx
+++ src/features/BankAccount/screens/Detail/index.tsx
@@ -1,18 +1,41 @@
+/* eslint-disable unicorn/prevent-abbreviations */
 /* eslint-disable unicorn/consistent-function-scoping */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Card, Select } from 'antd'
 import TextField from '@components/atoms/TextField'
 import Input from '@components/atoms/Input'
 import { ChangeEvent, useState } from 'react'
 import Button from '@components/atoms/Button'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
+import {
+  useFetchAllBank,
+  useFetchDetailBankAccount,
+  useUpdateBankAccount
+} from '@features/BankAccount/hooks'
+import { toast } from 'react-toastify'
+import { DetailBankAccountResponse } from '@features/BankAccount/BankAccount.type'
 
 const BankAccountDetail = () => {
   const history = useHistory()
+  const { bankAccountId }: { bankAccountId: string } = useParams()
   const { Option } = Select
   const [form, setForm] = useState({
-    name: '',
-    numberAccount: '',
-    bankName: ''
+    bankOwner: '',
+    bankNumber: '',
+    bankCode: ''
+  })
+  const { data: optionsAllBank } = useFetchAllBank({ page: 1, limit: 100 })
+  const { mutate: updateBankAccount } = useUpdateBankAccount()
+  useFetchDetailBankAccount(Number(bankAccountId), {
+    onSuccess: ({ data: { Data } }: DetailBankAccountResponse) => {
+      setForm(prevState => ({
+        ...prevState,
+        bankOwner: Data.bank_owner,
+        bankNumber: Data.bank_number,
+        bankCode: Data.bank_code
+      }))
+    }
   })
 
   const onChangeInput = (event: ChangeEvent<HTMLInputElement>, key: string) => {
@@ -22,33 +45,47 @@ const BankAccountDetail = () => {
     }))
   }
 
-  const onSelectBankName = (value: string, key: string) => {
-    console.info(key, value)
+  const onSelectBankCode = (value: string) => {
+    setForm(previousState => ({ ...previousState, bankCode: value }))
   }
 
-  const handleSubmit = () => console.info('SUBMIT')
+  const handleSubmit = () => {
+    updateBankAccount(
+      {
+        store_id: Number(bankAccountId),
+        bank_code: form.bankCode,
+        bank_owner: form.bankOwner,
+        bank_number: form.bankNumber
+      },
+      {
+        onSuccess: () => {
+          history.push('/bank-account')
+          toast.success('Edit Bank Account Success')
+        }
+      }
+    )
+  }
 
   return (
     <div>
       <Card title="Nama Merchant">
         <TextField label="Nama Rekening">
-          <Input value={form.name} onChange={event => onChangeInput(event, 'name')} />
+          <Input value={form.bankOwner} onChange={event => onChangeInput(event, 'bankOwner')} />
         </TextField>
         <TextField label="No Rekening">
-          <Input
-            value={form.numberAccount}
-            onChange={event => onChangeInput(event, 'numberAccount')}
-          />
+          <Input value={form.bankNumber} onChange={event => onChangeInput(event, 'bankNumber')} />
         </TextField>
         <TextField label="Nama Bank">
           <Select
             className="w-full"
             optionFilterProp="children"
-            value={form.bankName}
+            value={form.bankCode}
             placeholder="Pilih Nama Bank"
-            onChange={value => onSelectBankName(value, 'bankName')}
+            onChange={value => onSelectBankCode(value)}
           >
-            <Option value="1">Name</Option>
+            {optionsAllBank?.map(item => (
+              <Option value={item.code}>{item.name}</Option>
+            ))}
           </Select>
         </TextField>
       </Card>
